a9afb941be7edde8dcd9d207d5ba6041c9f7792c
DevThomas <fremen1990@gmail.com>
2022-09-16 15:39:25 +0200

    EventForm - initial form tests

--- client/src/EventForm.test.tsx
+++ client/src/EventForm.test.tsx
@@ -8,4 +8,45 @@ describe("<EventForm/>", () => {
     const header = screen.getByRole("heading", { name: "Create Event" });
     expect(header).toBeInTheDocument();
   });
+
+  it("has First name input", () => {
+    render(<EventForm />);
+    const input = screen.getByLabelText("First name");
+    expect(input).toBeInTheDocument();
+  });
+
+  it("has Last name input", () => {
+    render(<EventForm />);
+    const input = screen.getByLabelText("Last name");
+    expect(input).toBeInTheDocument();
+  });
+
+  it("has Email input", () => {
+    render(<EventForm />);
+    const input = screen.getByLabelText("Email");
+    expect(input).toBeInTheDocument();
+  });
+
+  it("has Date input", () => {
+    render(<EventForm />);
+    const input = screen.getByLabelText(/date/i);
+    expect(input).toBeInTheDocument();
+  });
+
+  it("has 'Event Date' input with type date", () => {
+    render(<EventForm />);
+    const input = screen.getByLabelText(/date/i);
+    expect(input).toHaveAttribute("type", "date");
+  });
+
+  it("has Add Event button", () => {
+    render(<EventForm />);
+    const button = screen.getByRole("button", { name: "Add Event" });
+    expect(button).toBeInTheDocument();
+  });
+  it("disables button initially", () => {
+    render(<EventForm />);
+    const button = screen.getByRole("button", { name: "Add Event" });
+    expect(button).toBeDisabled();
+  });
 });
