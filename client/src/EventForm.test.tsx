04e7752dacffaa3c81f1ecb5d95bd98cad852e8b
DevThomas <fremen1990@gmail.com>
2022-09-16 15:57:34 +0200

    EventForm - Layout tests refactoring

--- client/src/EventForm.test.tsx
+++ client/src/EventForm.test.tsx
@@ -3,50 +3,46 @@ import { render, screen } from "@testing-library/react";
 import EventForm from "./EventForm";
 
 describe("<EventForm/>", () => {
-  it("has header", () => {
-    render(<EventForm />);
-    const header = screen.getByRole("heading", { name: "Create Event" });
-    expect(header).toBeInTheDocument();
-  });
+  describe("Layout - when the form is rendered", () => {
+    beforeEach(() => {
+      render(<EventForm />);
+    });
 
-  it("has First name input", () => {
-    render(<EventForm />);
-    const input = screen.getByLabelText("First name");
-    expect(input).toBeInTheDocument();
-  });
+    it("has header with name: 'Create Event'", () => {
+      const header = screen.getByRole("heading", { name: "Create Event" });
+      expect(header).toBeInTheDocument();
+    });
 
-  it("has Last name input", () => {
-    render(<EventForm />);
-    const input = screen.getByLabelText("Last name");
-    expect(input).toBeInTheDocument();
-  });
+    it.each`
+      label
+      ${"First name"}
+      ${"Last name"}
+      ${"Email"}
+      ${"Event Date"}
+    `("has input field for $label", ({ label }) => {
+      const input = screen.getByLabelText(label);
+      expect(input).toBeInTheDocument();
+    });
 
-  it("has Email input", () => {
-    render(<EventForm />);
-    const input = screen.getByLabelText("Email");
-    expect(input).toBeInTheDocument();
-  });
+    it.each`
+      label           | type       | id
+      ${"First name"} | ${"text"}  | ${"firstName"}
+      ${"Last name"}  | ${"text"}  | ${"lastName"}
+      ${"Email"}      | ${"email"} | ${"email"}
+      ${"Event Date"} | ${"date"}  | ${"eventDate"}
+    `("has input field for '$label' with type '$type'", ({ label, type }) => {
+      const input = screen.getByLabelText(label);
+      expect(input).toBeInTheDocument();
+      expect(input).toHaveAttribute("type", type);
+    });
 
-  it("has Date input", () => {
-    render(<EventForm />);
-    const input = screen.getByLabelText(/date/i);
-    expect(input).toBeInTheDocument();
-  });
-
-  it("has 'Event Date' input with type date", () => {
-    render(<EventForm />);
-    const input = screen.getByLabelText(/date/i);
-    expect(input).toHaveAttribute("type", "date");
-  });
-
-  it("has Add Event button", () => {
-    render(<EventForm />);
-    const button = screen.getByRole("button", { name: "Add Event" });
-    expect(button).toBeInTheDocument();
-  });
-  it("disables button initially", () => {
-    render(<EventForm />);
-    const button = screen.getByRole("button", { name: "Add Event" });
-    expect(button).toBeDisabled();
+    it("has Add Event button", () => {
+      const button = screen.getByRole("button", { name: "Add Event" });
+      expect(button).toBeInTheDocument();
+    });
+    it("disables button initially", () => {
+      const button = screen.getByRole("button", { name: "Add Event" });
+      expect(button).toBeDisabled();
+    });
   });
 });
