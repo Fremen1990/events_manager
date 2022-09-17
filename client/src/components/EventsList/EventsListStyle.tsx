20c5f2b232cf1f63951b853ba2f95b1b1cb15e12
DevThomas <fremen1990@gmail.com>
2022-09-17 18:50:34 +0200

    EventsList styling

--- client/src/components/EventsList/EventsListStyle.tsx
+++ client/src/components/EventsList/EventsListStyle.tsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
 const EventsListsStyle = styled.form`
+  min-height: 80vh;
+  min-width: 60vw;
   border: 1px solid #ccc;
   margin: 30px;
   padding: 30px;
   display: flex;
   flex-direction: column;
+  align-items: center;
   background-color: azure;
 `;
 
