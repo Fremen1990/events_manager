c1e836c4cbc5179348a6d93bf63fb4575d26cffa
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-10 15:22:38 +0100

    :rocket: CI update
    :rocket: Update lock file
    
    :construction:  CI config
    
    :construction:  CI config
    
    :construction:  CI config
    
    :construction:  CI config
    
    :construction:  CI config
    
    :construction:  CD Vercel config
    
    :construction:  CD Vercel config
    
    :construction:  CD Vercel config
    
    :construction:  CD Vercel config
    
    :construction:  CD Vercel config 100
    
    :construction:  CD Vercel config 100
    
    :construction:  CD Vercel config 100
    
    :construction:  CD Vercel config 101
    
    :construction:  CD Vercel config 101
    
    :construction:  CD Vercel config 102
    
    :construction:  CD Vercel config 103
    
    :construction:  CD Vercel config 103
    
    :construction:  Heroku
    
    :construction:  Heroku
    
    :construction:  Heroku
    
    :construction:  Heroku
    
    :construction:  Heroku
    
    :construction:  Heroku
    
    :construction:  Heroku
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :construction:  Heroku CD 11
    
    :memo: ReadMe Update
    
    :construction: Tests fix
    
    :construction: Tests fix
    
    :rocket: Storybook CD
    
    :rocket: Storybook CD
    
    :rocket: Storybook CD
    
    :test_tube: Add Coverage for CI tests
    
    :test_tube: Fix typo for tests
    
    :test_tube: Tests CI
    
    :test_tube: Tests CI false
    
    :rocket: Fix storybook CI and tests
    
    :rocket: Tests fix
    
    :rocket: Tests fix
    
    :rocket: Tests fix
    
    :rocket: Tests fix
    
    :rocket: Add Web CD
    
    :rocket: Add Web CD
    
    :rocket: Tests CI skip and fix CD
    
    :rocket: Deployment CI CD polishes
    
    :construction: Coverate deploy works
    
    :construction: Coverate deploy works
    
    :construction: Coverate deploy works
    
    :construction: Coverage Icov fix
    
    :construction: Coverage Icov fix
    
    :construction: Coverage Icov fix
    
    :construction: Coverage Icov fix
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :construction: Fix tests CI coverage
    
    :memo: Update ReadMe
    
    :alien: Publish all in one
    
    :alien: Publish all in one
    
    :alien: Publish all in one
    
    Update ReadMe
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    CD update
    
    BE vs FE connection check
    
    BE vs FE connection check
    
    BE vs FE connection check
    
    BE vs FE connection check

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -14,7 +14,7 @@ describe('AppController', () => {
   });
 
   describe('getData', () => {
-    it('should return "Welcome to api!"', () => {
+     it('should return "Welcome to api!"', () => {
       const appController = app.get<AppController>(AppController);
       expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
     });
